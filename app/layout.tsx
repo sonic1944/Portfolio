dd86506e4c7175781334cb0f25f866089bbfc25c
sonic1944 <159346080+sonic1944@users.noreply.github.com>
2026-04-29 20:39:12 +0200

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,7 +37,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         </video>
 
         {children}
-
+      <Analytics />
       </body> 
     </html>
   );
